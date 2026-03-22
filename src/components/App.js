05d86b2fe72a1465e4108b8922c8d046d65ca696
Ronison2589 <ronison.silva1989@gmail.com>
2026-03-22 12:06:16 -0300

    Add App.js for main React application

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Analyzer from './Analyzer';
+
+function App() {
+    return (
+        <div>
+            <h1>Analizador OB</h1>
+            <Analyzer />
+        </div>
+    );
+}
+
+export default App;
